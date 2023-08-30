66441cd68e4a816de781ef126e8cbb7f3b8ce3d5
Mathilde SEGAUD <mathilde.segaud@cerema.fr>
2023-08-30 17:25:36 +0200

    inutile

--- Doc_de_travail/TestFv.py
+++
@@ -1,7 +0,0 @@
-from DetectVegetationFormStratum import detectInTreeStratum
-from Lib_postgis import *
-
-connexion = openConnection('etape2', user_name='postgres', password='', ip_host='localhost', num_port='5432', schema_name='donneelidar')
-cursor =  connexion.cursor()
-
-detectInTreeStratum(connexion, 'sgts_veg4')
