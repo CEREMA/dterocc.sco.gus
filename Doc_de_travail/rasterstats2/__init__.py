a599568ed3c1d63bba3dbf5e3a6b7a6174a889e1
Mathilde SEGAUD <mathilde.segaud@cerema.fr>
2023-09-20 14:19:30 +0200

    restructuration

--- Doc_de_travail/rasterstats2/__init__.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-from .main import raster_stats, stats_to_csv
-from .utils import RasterStatsError
-
-__all__ = ['raster_stats', 'stats_to_csv', 'RasterStatsError']
-
